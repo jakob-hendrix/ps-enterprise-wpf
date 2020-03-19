7c8c17fd4890076081285abdcb9493d8e0e0e25b
jakob-hendrix <jhendrix001@gmail.com>
2020-03-19 11:14:33 -0500

    Added basic test data service and interface

---
+++ src/FriendOrganizer/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: load from a database
+            yield return new Friend {FirstName = "Twinkle", LastName = "Humperdink"};
+            yield return new Friend {FirstName = "Jaque", LastName = "Perrie"};
+            yield return new Friend {FirstName = "Alastar", LastName = "Guy"};
+            yield return new Friend {FirstName = "Jill", LastName = "Valentine"};
+        }
+    }
+}
